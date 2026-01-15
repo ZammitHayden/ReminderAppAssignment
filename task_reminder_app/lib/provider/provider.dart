b84d64b4cfec007220b288d3b3e978e5c15bccc8
ZammitHayden <haydenzam@gmail.com>
2026-01-15 16:12:30 +0100

    Commit 3: TaskProvider with ChangeNotifier (RU4)

---
+++ task_reminder_app/lib/provider/provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../models/task.dart';
+
+class TaskProvider extends ChangeNotifier {
+  List<Task> _tasks = [];
+  List<Task> get tasks => _tasks;
+
+  void addTask(String title) {
+    _tasks.add(Task(title: title));
+    notifyListeners();
+  }
+
+  void toggleTask(int index) {
+    _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    notifyListeners();
+  }
+}
